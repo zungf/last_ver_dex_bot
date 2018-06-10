dc42f9b6b75ac45e364bc2b4c8a20075a49530b4
mlgrni <37967753+mlgrni@users.noreply.github.com>
2018-06-11 00:41:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -432,43 +432,4 @@ client.on('message', message => {
 
 
 
-
-var prefix = "^";
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-// -say
-  if (command === "say") {
-          message.delete()
-    message.channel.sendMessage(args.join(" ")).catch(console.error);
-  }
-  
- 
-
-if (command == "embed") {
-    let say = new Discord.RichEmbed()
-    .setDescription(args.join("  "))
-    .setColor(0x23b2d6)
-    message.channel.sendEmbed(say);
-    message.delete();
-  }
-
-
-});
-
-
-
-
-
-
-
-
-
-
 client.login(process.env.BOT_TOKEN);

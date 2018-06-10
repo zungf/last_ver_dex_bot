c48e44e567cc33003fbc48ce1473d3bbfc13b924
mlgrni <37967753+mlgrni@users.noreply.github.com>
2018-06-11 00:54:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -425,6 +425,20 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+});
 
 
 
